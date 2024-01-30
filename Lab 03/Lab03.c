b174730e83334966200bbd4bd66b4c98b3fb90a8
Bharath-0410 <137025178+Bharath-0410@users.noreply.github.com>
2024-01-29 20:32:31 -0600

    Update Lab03.c

--- Lab 03/Lab03.c
+++ Lab 03/Lab03.c
@@ -2,51 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+// Insertion fucnction created. 
+// here the function implements the insertion algorithm on the array of strings. 
+// here we use Strcmp to compare the strings. 
 void insertion(char *sp[], int n) {
-    int b, l;
-    for (b = 1; b < n; b++) {
-        char *val = sp[b];
-        for (l = b - 1; l >= 0 && strcmp(sp[l], val) > 0; l--) {
-            sp[l + 1] = sp[l];
+    int x, y;
+    for (x = 1; x < n; x++) {
+        char *val = sp[x];
+        for (y = x - 1; y >= 0 && strcmp(sp[y], val) > 0; y--) {
+            sp[y + 1] = sp[y];
         }
-        sp[l + 1] = val;
+        sp[y + 1] = val;
     }
 }
 
-void readStrings(char *sp[], int n) {
-    printf("Enter %d strings:\n", n);
-    for (int i = 0; i < n; i++) {
+
+// Function for readstrings created.
+// this function is used to read the n number of strings created dynamically .  
+void readStrings(char *sp[], int k) {
+    printf("Enter %d strings:\n", k);
+    for (int i = 0; i < k; i++) {
         char buffer[100]; // Adjust the size based on your requirements
         scanf("%s", buffer);
         sp[i] = strdup(buffer);
     }
 }
 
-void displayStrings(char *sp[], int n) {
+
+// function for displaystrings created. 
+// here this function helps in displaying the strings one by one.
+void displayStrings(char *sp[], int l) {
     printf("Sorted strings:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < l; i++) {
         printf("%s\n", sp[i]);
     }
 }
 
+
+// main funnction started.
+// the main function is created by using the above mentioned functions. 
 int main() {
     int z;
     // Enter the number of array elements
     printf("Enter the number of array elements: ");
-    scanf("%d", &z);
+    scanf("%d", &z); // reading the number of array elements 
 
     char *sp[z]; // Declare an array of character pointers
 
     // Reading the array elements (strings) and allocating memory dynamically
     readStrings(sp, z);
 
-    // Initiating the insertion sorting
+    // Initiating the insertion sorting for the given array. 
     insertion(sp, z);
 
-    // Displaying the sorted strings
+    // Displaying the sorted strings 
     displayStrings(sp, z);
 
-    // Freeing the dynamically allocated memory
+    // Freeing the dynamically allocated memory after finishing the insertion algorithm. 
     for (int i = 0; i < z; i++) {
         free(sp[i]);
     }
